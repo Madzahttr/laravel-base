29a698948456448b37ac3c3419ee77ac50c78171
Madzahttr <24141433+Madzahttr@users.noreply.github.com>
2024-12-21 01:31:59 +1300

    admin stuff

--- resources/js/Pages/Admin/Overview.vue
+++ resources/js/Pages/Admin/Overview.vue
@@ -11,8 +11,14 @@ import { Head } from '@inertiajs/vue3';
         <template #subtitle>A quick glance</template>
         <template #path>Admin > Overview</template>
 
-        <div class="w-full">
-            
+        <div class="w-full mt-1 px-8">
+            <div class="relative overflow-hidden bg-white shadow-md dark:bg-gray-800 sm:rounded-t-lg border-t-4 border-blue-600">
+                <div class="items-center justify-between p-4 space-y-3 sm:flex sm:space-y-0 sm:space-x-4">
+                    <div>
+                        <span class="mr-3 font-semibold dark:text-white text-lg">System Information</span>
+                    </div>
+                </div>
+            </div>
         </div>
     </AdminLayout>
 </template>
--- resources/js/Pages/Admin/Settings.vue
+++ resources/js/Pages/Admin/Settings.vue
@@ -11,8 +11,14 @@ import { Head } from '@inertiajs/vue3';
         <template #subtitle>Adjust site settings</template>
         <template #path>Admin > Settings</template>
         
-        <div class="w-full">
-            
+        <div class="w-full mt-1 px-8">
+            <div class="relative overflow-hidden bg-white shadow-md dark:bg-gray-800 sm:rounded-t-lg border-t-4 border-blue-600">
+                <div class="items-center justify-between p-4 space-y-3 sm:flex sm:space-y-0 sm:space-x-4">
+                    <div>
+                        <span class="mr-3 font-semibold dark:text-white text-lg">General</span>
+                    </div>
+                </div>
+            </div>
         </div>
     </AdminLayout>
 </template>
